a8caeee016d0139e71af4b53fcf2fca490375a83
NMontesino <namontesino@gmail.com>
2019-12-19 08:57:31 -0500

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  post '/users/login', :to => 'users#login'
-  get '/users/auto_login', :to => 'users#auto_login'
-  resources :tasks
-  resources :users
+  post '/api/v1/users/login', :to => 'api/v1/users#login'
+  get '/api/v1/users/auto_login', :to => 'api/v1/users#auto_login'
+  namespace :api do
+    namespace :v1 do
+      resources :tasks
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
