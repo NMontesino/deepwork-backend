a9e403b59a7e644742bb87e1e9f401305e6b48c9
NMontesino <namontesino@gmail.com>
2019-12-19 13:44:47 -0500

    Reverting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  post '/api/v1/users/login', :to => 'api/v1/users#login'
-  get '/api/v1/users/auto_login', :to => 'api/v1/users#auto_login'
-  namespace :api do
-    namespace :v1 do
-      resources :tasks
-      resources :users
-    end
-  end
+  post '/users/login', :to => 'users#login'
+  get '/users/auto_login', :to => 'users#auto_login'
+  resources :tasks
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
