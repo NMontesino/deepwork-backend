4cb5cb3eb0a8cdaa5e5cd6be58434651efca2b2d
NMontesino <namontesino@gmail.com>
2019-12-19 09:26:37 -0500

    Starting New App

---
+++ public/precache-manifest.442a4c6e91740dfe5b86d1e8ba00b6fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586886b14bc03fc52438962d63b6f622",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78050cd2631625afdf1b",
+    "url": "/static/css/main.16ea595c.chunk.css"
+  },
+  {
+    "revision": "8884a71195bb7edc513e",
+    "url": "/static/js/2.05bb5a85.chunk.js"
+  },
+  {
+    "revision": "78050cd2631625afdf1b",
+    "url": "/static/js/main.c2df51d0.chunk.js"
+  },
+  {
+    "revision": "cafdbcfde535409d0945",
+    "url": "/static/js/runtime-main.bf07e05f.js"
+  },
+  {
+    "revision": "a76ce23375d68a423cd6bb0161a674a0",
+    "url": "/static/media/Mindfulness Bell - A 5 Minute Mindfulness Meditation.a76ce233.wav"
+  },
+  {
+    "revision": "140e6d8dc83961efad03c8c78c7cf279",
+    "url": "/static/media/noun_Play in circle_2433769.140e6d8d.svg"
+  },
+  {
+    "revision": "692f829246921e7d85befe7d4896ea63",
+    "url": "/static/media/noun_Refresh_2433739.692f8292.svg"
+  },
+  {
+    "revision": "2dc19fd18f08c1113bcace5ac5c70a84",
+    "url": "/static/media/noun_pause in circle_2433738.2dc19fd1.svg"
+  },
+  {
+    "revision": "5a4b55a822007de0515c41bb0924348a",
+    "url": "/static/media/service-bell_daniel_simion.5a4b55a8.wav"
+  }
+]);
